57321eca82650017c70ad28e4489a148aa152489
saadstar <heshamsaad151t@gmail.com>
2024-02-05 03:09:18 +0200

    env handler

--- api/config/dbConnection.js
+++ api/config/dbConnection.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB);
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: process.env.DB_NAME,
+    });
     console.log("Connected to database.");
   } catch (err) {
     console.log("Failed to connect to database!");
